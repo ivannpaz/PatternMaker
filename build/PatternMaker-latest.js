f509edd90423d83a2d48ef7615cca766a8d36796
ivan <ivannpaz@gmail.com>
2013-12-18 00:06:54 +0100

    Rename the generated files

--- build/PatternMaker-latest.js
+++ build/PatternMaker-latest.js
@@ -1,9 +1,14 @@
 /**
  * Package:    PatternMaker
- * Build:      2013-12-17
+ * Build:      2013-12-18
  * Version:    0.1.0
  */
 (function($) {
+    var Utils = {
+        getRandom: function(max) {
+            return Math.floor(Math.random() * max);
+        }
+    };
     var RandomColor = function() {
         function RandomColor(palette) {
             this.setPalette(palette);
@@ -33,9 +38,26 @@
         };
         return RandomColor;
     }();
-    var Utils = {
-        getRandom: function(max) {
-            return Math.floor(Math.random() * max);
+    (function($) {
+        var pluginName = "patternMaker", pluginDefaults = {
+            palette: []
+        };
+        function Plugin(element, options) {
+            this.element = element;
+            this.$element = $(element);
+            this.options = $.extend({}, pluginDefaults, options);
+            this.init();
         }
-    };
+        Plugin.prototype.init = function() {
+            this._palette = new RandomColor(this.options.palette);
+        };
+        $.fn[pluginName] = function(options) {
+            return this.each(function() {
+                if (!$.data(this, "plugin_" + pluginName)) {
+                    $.data(this, "plugin_" + pluginName);
+                    new Plugin(this, options);
+                }
+            });
+        };
+    })(jQuery);
 })(window.jQuery);
