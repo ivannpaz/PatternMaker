26b29d252c5b79e991ae31f8b338a014d38dc802
ivan <ivannpaz@gmail.com>
2013-12-17 20:52:24 +0100

    Add example basic files

---
+++ examples/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    var colorPalettes = [
+        ['#69370B', '#F2B66C', '#E06011', '#FCA71E', '#004400'],
+        ['#EEEECA', '#88A890', '#5E7480', '#5A4B76', '#3F3755'],
+        ['#F7A600', '#B6BE2A', '#398FB6', '#E96F3B', '#6B6759'],
+        ['#D0DF82', '#ECD026', '#FD4F4E', '#A11931', '#2A1B14']
+    ];
+
+    var $board = $('body > article > section');
+
+    $board.html('<p>Testing...<p>');
+});
