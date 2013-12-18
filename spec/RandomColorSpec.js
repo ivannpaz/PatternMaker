6bd0b4cb9d83bcea5b0f274b58f6dcd0c6d23d00
ivan <ivannpaz@gmail.com>
2013-12-18 23:03:29 +0100

    Add basic specs to the main functionality of getColor

--- spec/RandomColorSpec.js
+++ spec/RandomColorSpec.js
@@ -1,6 +1,7 @@
 /**
  * Load Subject
  */
+eval(require('fs').readFileSync('./src/Utils.js', 'utf8'));
 eval(require('fs').readFileSync('./src/RandomColor.js', 'utf8'));
 
 /**
@@ -8,8 +9,26 @@ eval(require('fs').readFileSync('./src/RandomColor.js', 'utf8'));
  */
 describe("RandomColor", function() {
 
-    it("should exist", function() {
-        var classy = new RandomColor(['whatevs']);
+    var samplePalette = ['#f00', '#ff0', '#fff'],
+        subject;
+
+    beforeEach(function() {
+        subject = new RandomColor(samplePalette);
+    });
+
+    it("will be initialized with a given palette", function() {
+        expect(subject._lastColor).toBe(false);
+        expect(subject._delivered).toEqual([]);
+        expect(subject._palette).toEqual(samplePalette);
+    });
+
+    it("will return different color on each call", function() {
+        var color1 = subject.getColor(),
+            color2 = subject.getColor(),
+            color3 = subject.getColor();
+
+        expect(color2).not.toEqual(color1);
+        expect(color3).not.toEqual(color2);
     });
 
 });
